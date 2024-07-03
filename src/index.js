c02c612279ecf052f40e4c1ee15e9c697f8e77c9
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-03 20:06:53 -0300

    update content

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-//borda verde
+/*
+1 - Aquele p de campo obrigatório deve estar abaixo de todos os inputs
+2 - Pra tu manipular os inputs com mais facilidade, todos deve ter a mesma classe, inclusive o textarea 
+3 - Depois a ideia é: os campos devem ser validados após o envio do mesmo. Então a partir dai tu monta a lógica, pegando o elemento do formulário, adicionando um evento de submit e fazer a manipulação dos campos dentro desse evento
+4 - Depois de obter TODOS os elementos, é necessário percorrer todos usando o forEach e fazer a verificação se o input está vazio ou não usando .value === ""
 
+*/
