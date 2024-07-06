0f23c0d8cbbd1b6dbafc8a395c26f7461e58c08e
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-06 19:10:42 -0300

    update content

--- src/index.js
+++ src/index.js
@@ -1,21 +1,63 @@
 const form = document.getElementById('form')
 const inputs = document.querySelectorAll('.input');
 
+function validateInput(input) {
+
+    if (input.value.trim() === '') {
+
+        input.classList.add('invalido')
+        input.classList.remove('valido')
+        input.nextElementSibling.classList.add('mostrar')
+
+    } else {
+
+        input.classList.add('valido')
+        input.classList.remove('invalido')
+        input.nextElementSibling.classList.remove('mostrar')
+    }
+}
+
+//verificar preenchimento
 form.addEventListener('submit', function (event) {
+
     event.preventDefault();
 
     inputs.forEach(function (input) {
-
-        if (input.value.trim() === '') {
-            input.classList.add('invalido')
-            input.classList.remove('valido')
-            input.nextElementSibling.classList.add('mostrar')
-            
-
-        } else {
-            input.classList.add('valido')
-            input.classList.remove('invalido')
-            input.nextElementSibling.classList.remove('mostrar')
-        }
+        validateInput(input)
     })
 })
+
+//colocar verde
+inputs.forEach(function (input) {
+
+    input.addEventListener('input', function () {
+        validateInput(input);
+    })
+})
+
+
+
+
+//Código antigo
+
+// form.addEventListener('submit', function (event) {
+//     event.preventDefault();
+
+//     inputs.forEach(function (input) {
+
+//         if (input.value.trim() === '') {
+//             input.classList.add('invalido')
+//             input.classList.remove('valido')
+//             input.nextElementSibling.classList.add('mostrar')
+
+
+//         } else {
+//             input.classList.add('valido')
+//             input.classList.remove('invalido')
+//             input.nextElementSibling.classList.remove('mostrar')
+//         }
+//     })
+// })
+
+
+
